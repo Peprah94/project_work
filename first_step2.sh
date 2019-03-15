b434f5165c7b09b991b3d151777483e3e78e1dc9
Peprah94 <31469850+Peprah94@users.noreply.github.com>
2019-03-15 08:29:33 +0100

    Create first_step2.sh

---
+++ first_step2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --gres=gpu:1
+#SBATCH --job-name="first_step_2"
+#SBATCH --output=first step 2
+#SBATCH --partition=EPICALL
+
+cd /home/kwakupa/project_work/
+ 
+source /home/kwakupa/masters_thesis/load_module.sh
+
+Rscript /home/kwakupa/project_work/first_step_example2.R --save
