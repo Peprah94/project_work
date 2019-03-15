1679c23554de7b0931e21a843235be7319a5a7c2
Peprah94 <31469850+Peprah94@users.noreply.github.com>
2019-03-15 08:31:26 +0100

    Create first_step4.sh

---
+++ first_step4.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --gres=gpu:1
+#SBATCH --job-name="first_step_4"
+#SBATCH --output=first step 4
+#SBATCH --partition=EPICALL
+
+cd /home/kwakupa/project_work/
+ 
+source /home/kwakupa/masters_thesis/load_module.sh
+
+Rscript /home/kwakupa/project_work/first_step_example4.R --save
